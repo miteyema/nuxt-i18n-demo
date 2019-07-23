b57d6f62a585d3e5ffbe3400c56edc07238afadf
Tim Ameye <miteyema@users.noreply.github.com>
2019-07-23 10:02:54 +1000

    add eslint-plugin-vue-i18n

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,16 @@ module.exports = {
     'prettier/vue',
     'plugin:prettier/recommended',
     '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'plugin:vue-i18n/recommended'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
-  rules: {
+  rules: {},
+  settings: {
+    // See https://kazupon.github.io/eslint-plugin-vue-i18n/started.html#usage
+    'vue-i18n': {
+      localeDir: './locales/*.json' // extention is glob formatting!
+    }
   }
 }
