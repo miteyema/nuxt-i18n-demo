3a616626978fb5b698e62f7f07a81b90100adaab
Tim Ameye <miteyema@users.noreply.github.com>
2020-09-08 20:51:02 +1000

    fix nuxt-i18n

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,14 +1,10 @@
 <!-- eslint-disable vue-i18n/no-raw-text -->
 <template>
   <div>
-    <h1 class="title">
-      Privacy
-    </h1>
+    <h1 class="title">Privacy</h1>
     <h2 class="subtitle">
       As I don't have time to translate, this will just stay in English.
-      <nuxt-link :to="localePath('index')">
-        Go back
-      </nuxt-link>
+      <nuxt-link :to="localePath('index')">Go back</nuxt-link>
     </h2>
   </div>
 </template>
