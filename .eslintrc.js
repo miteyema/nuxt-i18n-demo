6227ebc3d9bf6672d6e12ceaae9379984b0a556a
Tim <miteyema@users.noreply.github.com>
2021-07-25 20:36:22 +1000

    update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,16 @@ module.exports = {
     node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
   },
   extends: [
     '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
-    'plugin:vue-i18n/recommended',
+    'prettier',
+    'plugin:@intlify/vue-i18n/recommended',
   ],
-  plugins: ['prettier'],
+  plugins: [],
   // add your custom rules here
   rules: {},
   settings: {
